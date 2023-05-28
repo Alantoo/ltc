a2e48d8271ac7e8d12c2921f69a38f01268188b7
Andrii Kondratiuk <x.alantoo@gmail.com>
2023-05-28 14:41:12 +0300

    added ci-cd

--- packages/frontend/src/components/PriceBox/PriceBox.tsx
+++ packages/frontend/src/components/PriceBox/PriceBox.tsx
@@ -1,26 +1,33 @@
 import { Price } from './Price';
 import styles from './PriceBox.module.scss';
+
 type Price = {
   text: string;
 };
 const priceMock: Price[] = [
   {
-    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab adipisci animi beatae bland',
+    text:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab adipisci animi beatae bland',
   },
   {
-    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab adipisci animi beatae bland',
+    text:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab adipisci animi beatae bland',
   },
   {
-    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab adipisci animi beatae bland',
+    text:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab adipisci animi beatae bland',
   },
   {
-    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab adipisci animi beatae bland',
+    text:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab adipisci animi beatae bland',
   },
   {
-    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab adipisci animi beatae bland',
+    text:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab adipisci animi beatae bland',
   },
   {
-    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab adipisci animi beatae bland',
+    text:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab adipisci animi beatae bland',
   },
 ];
 export const PriceBox = () => {
