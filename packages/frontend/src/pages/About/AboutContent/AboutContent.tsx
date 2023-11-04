484675137f4e7e8a9c5997770ae128bfc5ea1bfe
shhhwepsss <xomai.ars@gmail.com>
2023-11-04 13:31:28 +0300

    change: text

--- packages/frontend/src/pages/About/AboutContent/AboutContent.tsx
+++ packages/frontend/src/pages/About/AboutContent/AboutContent.tsx
@@ -16,7 +16,7 @@ export const AboutContent = () => {
       <Paragraph
         title="The Money Lists"
         subtitle="The money lists work by using an algorithm to randomly rotate each member username through the money lists of our Global Money List website. All positions in the money lists randomly rotate every ten seconds. There are six separate Money Lists your username can rotate through. There’s a $1.00 Money List, $5.00 Money List, $10.00 Money List, $20.00 Money List, $50.00 Money List, and a $100.00 Money List you can be added to.
-        Here’s the cost to be added to our various Money Lists. All payments must be made in Bitcoin or Ethereum cryptocurrency!"
+        Here’s the cost to be added to our various Money Lists. All payments must be made in Bitcoin cryptocurrency!"
         subtitleTopMargin={5}
         subtitleLineHeight={'27px'}
         containerMargin={'30px auto 0 '}
@@ -24,9 +24,9 @@ export const AboutContent = () => {
       <ThreeColumnsPicture
         leftColumn={{
           list: [
-            '$100.00 Money List is $2.95 per 60-day entry. You must also pay two random members $100.00 each in cryptocurrency before you can be added to this Money List.',
-            '$50.00 Money List is $2.95 per 60-day entry. You must also pay two random members $50.00 each in cryptocurrency before you can be added to this Money List.',
-            '$20.00 Money List is $2.95 per 60-day entry. You must also pay two random members $20.00 each in cryptocurrency before you can be added to this Money List.',
+            '$100.00 Money List - You must pay two random members $100.00 each in Bitcoin before you can be added to this Money List. All entries are for 60 days.',
+            '$50.00 Money List - You must pay three random members $50.00 each in Bitcoin before you can be added to this Money List. All entries are for 60 days.',
+            '$20.00 Money List - You must pay four random members $20.00 each in Bitcoin   before you can be added to this Money List. All entries are for 60 days.',
           ],
           isList: true,
           lineContent: '',
@@ -34,9 +34,9 @@ export const AboutContent = () => {
         }}
         rightColumn={{
           list: [
-            '$10.00 Money List is $2.95 per 60-day entry. You must also pay two random members $10.00 each in cryptocurrency before you can be added to this Money List.',
-            '$5.00 Money List is $2.95 per 60-day entry. You must also pay two random members $5.00 each in cryptocurrency before you can be added to this Money List.',
-            '$1.00 Money List is $2.95 per 60-day entry. You must also pay two random members $1.00 each in cryptocurrency before you can be added to this Money List.',
+            '$10.00 Money List - You must pay five random members $10.00 each in Bitcoin before you can be added to this Money List. All entries are for 60 days.',
+            '$5.00 Money List - You must pay six random members $5.00 each in Bitcoin before you can be added to this Money List. All entries are for 60 days.',
+            '$1.00 Money List - You must pay seven random members $1.00 each in Bitcoin before you can be added to this Money List. All entries are for 60 days.',
           ],
           isList: true,
           lineContent: '',
@@ -48,19 +48,12 @@ export const AboutContent = () => {
         extraClass={styles.threeColumnContainer}
       />
       <p className={styles.earningWillBePaid}>
-        All earnings will be paid to you with one of the cryptocurrencies listed
-        below.
+        All earnings will be paid to you in Bitcoin cryptocurrency.
       </p>
       <div className={styles.cryptoCardsContainer}>
         <CryptoCard
           cryptoName="Bitcoin"
           img={bitcoin}
-          width={595}
-          height={373}
-        />
-        <CryptoCard
-          cryptoName="Ethereum"
-          img={ethereum}
           width={595}
           height={373}
         />
@@ -75,7 +68,19 @@ export const AboutContent = () => {
       <HowToBeAdded />
       <Paragraph
         title="Referral List"
-        subtitle="Building your referral list is the most important way to create residual income using this system. Here’s how it works! When someone click on your affiliate link and sign up, they’re automatically added to your referral list. When any member listed in your referral list pay to be added to any of our money lists, your username will be locked in the #1 position of their money list. Once clicked on You’ll be paid either $1.00, $5.00, $10.00, $20.00, $50.00, or $100.00 in cryptocurrency depending on which money list your referral decides to be added to. Your referral will now click on the required number of members usernames randomly rotating through the money list to have themselves added to that money list. There is no limit to how large your referral list can grow to be, so the larger the list the more income for you. Go to your homepage and copy your affiliate link to share with all of your family, friends, and network associates. This will rapidly grow your referral list causing you to earn unlimited amounts of income even long after you stopped promoting this system."
+        subtitle={`Building your referral list is the most important way to create residual income
+        using this system. Here’s how it works! When someone click on your affiliate link and sign
+         up, they’re automatically added to your referral list. When any member listed in your referral 
+         list pay to be added to any of our money lists, your username will be locked in the #1 position 
+         of their money list. Once clicked on You’ll be paid either $1.00, $5.00, $10.00, $20.00,
+          $50.00, or $100.00 in Bitcoin cryptocurrency depending on which money list your referral
+           decides to be added to. Your referral will now click on the required number of members 
+           usernames randomly rotating through the money list to have themselves added to that money
+            list. There is no limit to how large your referral list can grow to be, so the larger th
+            e list the more income for you. Go to your homepage and copy your affiliate link to share
+             with all of your family, friends, and network associates. This will rapidly grow your re
+             ferral list causing you to earn unlimited amounts of income even long after you stopped 
+             promoting this system.`}
         subtitleTopMargin={5}
         subtitleLineHeight="26px"
         containerMargin="10px auto 0"
--- packages/frontend/src/pages/Home/Home.tsx
+++ packages/frontend/src/pages/Home/Home.tsx
@@ -124,15 +124,15 @@ const HomeView = ({ classes }: HomeProps) => {
         <button className="buttonInImg">Join Now</button>
       </MainImgWithContent>
       <Paragraph
-        title="Welcome to Global Money List the easiest way to earn unlimited amounts of Bitcoin or Ethereum cryptocurrency daily."
-        subtitle="When you join Global Money List you can be added to any of our six money lists. Every time a member clicks on your username rotating through the money lists, you’ll be paid either$1.00, $5.00, $10.00, $20.00, $50.00, or $100.00 in Bitcoin or Ethereum. You’ll also receive an affiliate link to share with others and build your referral list which allows you to make money on autopilot."
+        title="Welcome to Global Money List the easiest way to earn unlimited amounts of Bitcoin cryptocurrency daily."
+        subtitle="When you join Global Money List you can be added to any of our six money lists. Every time a member clicks on your username rotating through the money lists, you’ll be paid either$1.00, $5.00, $10.00, $20.00, $50.00, or $100.00 in Bitcoin cryptocurrency. You’ll also receive an affiliate link to share with others and build your referral list which allows you to make money on autopilot."
         subtitleTopMargin={15}
         subtitleLineHeight="26.78px"
         containerMargin={'42px auto 0 117px'}
       />
       <Paragraph
         title=""
-        subtitle="What makes Global Money List system cheat proof is that the members select the winners who are randomly rotating through the money lists. Global Money List guarantees you as many as seven members will be paid prior to any member entering the money lists. Members who enter any of our money lists will consistently rotate for a whopping 60 days! Can you imagine walking into a casino and having the opportunity to pay a one-time fee to have unlimited play on a slot machine for 60 days. This is exactly what Global Money List is offering you with our one-of-a-kind residual income system. Our money lists automatically rotate our members username randomly every 10 seconds giving you the best possibility to make hundreds maybe even thousands of dollars in Bitcoin or Ethereum cryptocurrency every day! Our system is designed to deposit your winnings instantly into your cryptocurrency wallet."
+        subtitle="What makes Global Money List system cheat proof is that the members select the winners who are randomly rotating through the money lists. Global Money List guarantees you as many as seven members will be paid prior to any member entering the money lists. Members who enter any of our money lists will consistently rotate for a whopping 60 days! Can you imagine walking into a casino and having the opportunity to pay a one-time fee to have unlimited play on a slot machine for 60 days. This is exactly what Global Money List is offering you with our one-of-a-kind residual income system. Our money lists automatically rotate our members username randomly every 10 seconds giving you the best possibility to make hundreds maybe even thousands of dollars in Bitcoin cryptocurrency every day!Our system is designed to deposit your winnings instantly into your cryptocurrency wallet."
         subtitleTopMargin={0}
         subtitleLineHeight="26.78px"
         containerMargin={'35px auto 0 117px'}
@@ -150,8 +150,7 @@ const HomeView = ({ classes }: HomeProps) => {
       </div>
       <Paragraph
         title={`Referral List`}
-        subtitle={`Building your referral list is the most important way to create residual income using this system. Here’s how it works! When someone click on your affiliate link and sign up, they’reautomatically added to your referral list. When any member listed in your referral list pay to be added to any of our money lists, your username will be locked in the #1 position of theirmoney list. Once clicked on You’ll be paid either $1.00, $5.00, $10.00, $20.00, $50.00, or $100.00 in cryptocurrency depending on which money list your referral decides to be added to.Your referral will now click on the required number of members usernames randomly rotating through the money list to have themselves added to that money list. There is no limit tohow large your referral list can grow to be, so the larger the list the more income for you. Go to your homepage and copy your affiliate link to share with all of your family, friends, andnetwork associates. This will rapidly grow your referral list causing you to earn unlimited amounts of income even long after you stopped promoting this system.
-        `}
+        subtitle={`What makes Global Money List system cheat proof is that the members select the winners who are randomly rotating through the money lists. Global Money List guarantees you as many as seven members will be paid prior to any member entering the money lists. Members who enter any of our money lists will consistently rotate for a whopping 60 days! Can you imagine walking into a casino and having the opportunity to pay a one-time fee to have unlimited play on a slot machine for 60 days. This is exactly what Global Money List is offering you with our one-of-a-kind residual income system. Our money lists automatically rotate our members username randomly every 10 seconds giving you the best possibility to make hundreds maybe even thousands of dollars in Bitcoin cryptocurrency every day!Our system is designed to deposit your winnings instantly into your cryptocurrency wallet.`}
         subtitleTopMargin={3}
         subtitleLineHeight="31px"
         containerMargin="70px auto 0 71px"
